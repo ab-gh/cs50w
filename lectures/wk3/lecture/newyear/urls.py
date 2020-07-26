ad8fb2e87f34fe1f0ae28aceb70226a502a999a8
aejb <thomasbass2001@gmail.com>
2020-07-26 15:37:04 +0100

    Lecture 3 finished

--- lectures/wk3/lecture/newyear/urls.py
+++ lectures/wk3/lecture/newyear/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+app_name = "newyear"
 urlpatterns = [
     path("", views.index, name="index")
 ]
--- lectures/wk3/lecture/tasks/views.py
+++ lectures/wk3/lecture/tasks/views.py
@@ -1,10 +1,31 @@
+from django import forms
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
 
-tasks = ["foo", "bar", "baz"]
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label="New Task")
 
 def index(request):
+    if "tasks" not in request.session:
+        request.session['tasks'] = []
     return render(request, "tasks/index.html", {
-        "tasks": tasks
+        "tasks": request.session['tasks']
+    })
+
+def add(request):
+    if request.method == "POST":
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            task = form.cleaned_data['task']
+            request.session['tasks'] += [task]
+            return HttpResponseRedirect(reverse("tasks:index"))
+        else:
+            return render(request, "tasks/add.html", {
+                "form": form
+            })
+    return render(request, "tasks/add.html", {
+        "form": NewTaskForm()
     })
